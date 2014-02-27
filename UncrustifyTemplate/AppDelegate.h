306da2422bac65c26dfb03debadddd1f503bba1f
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2014-02-27 17:04:57 +0900

    Initial Commit

---
+++ UncrustifyTemplate/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UncrustifyTemplate
+//
+//  Created by Hiroyuki Onishi on 2/27/14.
+//  Copyright (c) 2014 Hiroyuki Onishi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UncrustifyTemplateTests/UncrustifyTemplateTests.m
@@ -0,0 +1,34 @@
+//
+//  UncrustifyTemplateTests.m
+//  UncrustifyTemplateTests
+//
+//  Created by Hiroyuki Onishi on 2/27/14.
+//  Copyright (c) 2014 Hiroyuki Onishi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UncrustifyTemplateTests : XCTestCase
+
+@end
+
+@implementation UncrustifyTemplateTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
